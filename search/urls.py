9c5ba012fe651c636fcaaa207566f568985d1fd8
Jonathan <jgargano@my.ccsu.edu>
2022-12-14 18:52:47 -0500

    Moved navbar to base.html. Added specific urls such as liked-playlist and logout. Added crispy forms to login. Made sure the spinning wheel is working and centered. Added margin/space under navbar. Search bar now works with either song or artist. Playlist displayed liked cards and deletes when clicking heart. Cleaned up the navbar, if not logged in displays login/register, if logged in will say "Welcome {user}!".

--- search/urls.py
+++ search/urls.py
@@ -4,14 +4,17 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.homepage),
-    path('show/', views.show, name='show'),
+    path('', views.show, name='show'),
     path('register/', views.register, name='register'),
     path('login/', views.myLogin, name='login'),
     path('liked/', views.liked, name='liked'),
-    path('show/playlist/', views.playlist, name='playlist'),
-]
+    path('playlist/', views.playlist, name='playlist'),
+    path('search/', views.search, name='search'),
+    path('search-playlist/', views.search, name='search-playlist'),
+    path('logout/', views.myLogin, name='logout'),
+    path('liked-playlist/', views.liked, name='liked-playlist'),
 
+]
 
 # Necessary for images to be loaded correctly
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
